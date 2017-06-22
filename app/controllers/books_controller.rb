23cf80d0bc27522e7c97a07772c65a75c3844211
Tilo Germer <tilo.germer@t-online.de>
2017-06-22 16:13:01 +0200

    associated books and users and improved index functionality

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,30 +1,24 @@
 class BooksController < ApplicationController
 	before_action :find_book, only: [:show, :edit, :update, :destroy]
-
 def index
 	@books = Book.all.order("created_at DESC")
 end
-
 def show
 end
 
 def new 
-	@book = Book.new
+	@book = current_user.books.build
 end
-
 def create
-	@book = Book.new(book_params)
-
+	@book = current_user.books.build(book_params)
 	if @book.save
 		redirect_to root_path
 	else
 		render 'new'
 	end
 end
-
 def edit
 end
-
 def update
 	if @book.update(book_params)
 		redirect_to book_path(@book)
@@ -32,12 +26,10 @@ def update
 		render 'new'
 	end
 end
-
 def destroy
 	@book.destroy
 	redirect_to root_path
 end
-
 private
 	def book_params
 		params.require(:book).permit(:title, :description, :author)
