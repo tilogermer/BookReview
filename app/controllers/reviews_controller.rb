6e34784e43ca46610fcdc05c9f509c73229f7620
Tilo Germer <tilo.germer@t-online.de>
2017-06-23 15:19:31 +0200

    added the reviews on the page and created edit form for reviews

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
 	before_action :find_book
+	before_action :find_review, only: [:edit, :update, :destroy]
 
 	def new
 		@review = Review.new
@@ -17,6 +18,18 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def edit
+		
+	end
+
+	def update
+		if @review.update(review_params)
+			redirect_to book_path(@book)
+		else
+			render 'edit'
+		end
+	end
+
 	private
 	def review_params
 		params.require(:review).permit(:rating, :comment)
@@ -24,6 +37,10 @@ class ReviewsController < ApplicationController
 
 	def find_book
 		@book = Book.find(params[:book_id])
+	end
+
+	def find_review
+		@review = Review.find(params[:id])
 	end
 
 end
