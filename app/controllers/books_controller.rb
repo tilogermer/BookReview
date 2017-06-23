c25f057cf6cea168fa02eba820207ae1ec6b2ec9
Tilo Germer <tilo.germer@t-online.de>
2017-06-23 06:45:46 +0200

    added categories and filter by categories in navbar

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,13 @@
 class BooksController < ApplicationController
 	before_action :find_book, only: [:show, :edit, :update, :destroy]
 def index
+	if params[:category].blank?
 	@books = Book.all.order("created_at DESC")
+	else
+		@category_id = Category.find_by(name: params[:category]).id
+		@books = Book.where(:category_id => @category_id).order("created_at DESC") 
+	end
+
 end
 def show
 end
@@ -20,8 +26,10 @@ def create
 	end
 end
 def edit
+	@categories = Category.all.map{|c| [c.name, c.id]}
 end
 def update
+	@book.category_id = params[:category_id]
 	if @book.update(book_params)
 		redirect_to book_path(@book)
 	else
