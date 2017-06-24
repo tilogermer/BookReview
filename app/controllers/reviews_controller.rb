981ddade9d4a63862fc365934ccb301707babc1e
Tilo Germer <tilo.germer@t-online.de>
2017-06-24 07:10:48 +0200

    added edit/delete for tthe reviews, but only for user who wrote it

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -30,6 +30,11 @@ class ReviewsController < ApplicationController
 		end
 	end
 
+	def destroy
+		@review.destroy
+		redirect_to book_path(@book)
+	end
+
 	private
 	def review_params
 		params.require(:review).permit(:rating, :comment)
